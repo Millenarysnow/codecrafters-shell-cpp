de63e704dc6037f3787c75b2a6f162ca9a5fd2b6
millenarysnow <1371684195@qq.com>
2026-01-12 16:50:44 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 bool Start = true;
 
+set<string> Commands;
+
 void Exit(vector<string>& Args)
 {
   Start = false;
@@ -21,6 +23,23 @@ void Echo(vector<string>& Args)
     cout << Args[i] << " ";
   }
   cout << Args[Args.size() - 1] << endl;
+}
+
+void Type(vector<string>& Args)
+{
+  if(Args.size() == 0)
+  {
+    cout << "error" << endl;
+  }
+
+  if(Commands.find(Args[0]) != Commands.end())
+  {
+    cout << Args[0] << " is a shell builtin" << endl;
+  }
+  else
+  {
+    cout << Args[0] << ": not found" << endl;
+  }
 }
 
 void SplitInput(string& Input, string& Command, vector<string>& Args)
@@ -40,11 +59,10 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  set<string> Commands;
   unordered_map<string, void(*)(vector<string>&)> Execute;
 
-  Commands.insert({"exit", "echo"});
-  Execute.insert({{"exit", &Exit}, {"echo", &Echo}});
+  Commands.insert({"exit", "echo", "type"});
+  Execute.insert({{"exit", &Exit}, {"echo", &Echo}, {"type", &Type}});
 
   while(Start)
   {
