ce719283ae638320ad3f2c5fe6d5f078ecf4b34d
millenarysnow <1371684195@qq.com>
2026-01-12 16:39:25 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,35 @@
 #include <vector>
 #include <set>
 #include <unordered_map>
+#include <sstream>
 
 using namespace std;
 
-set<string> Commands;
-unordered_map<string, void(*)()> Execute;
-
 bool Start = true;
 
-void Exit()
+void Exit(vector<string>& Args)
 {
   Start = false;
+}
+
+void Echo(vector<string>& Args)
+{
+  for (int i = 0; i < Args.size() - 1; i++)
+  {
+    cout << Args[i] << " ";
+  }
+  cout << Args[Args.size() - 1] << endl;
+}
+
+void SplitInput(string& Input, string& Command, vector<string>& Args)
+{
+  stringstream ss;
+  ss.str(Input);
+
+  string temp;
+  ss >> Command;
+  while(ss >> temp)
+    Args.push_back(temp);
 }
 
 int main() 
@@ -22,22 +40,30 @@ int main()
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  Commands.insert("exit");
-  Execute.insert({"exit", &Exit});
+  set<string> Commands;
+  unordered_map<string, void(*)(vector<string>&)> Execute;
+
+  Commands.insert({"exit", "echo"});
+  Execute.insert({{"exit", &Exit}, {"echo", &Echo}});
 
   while(Start)
   {
     std::cout << "$ ";
 
-    string inputCommand;
-    getline(cin, inputCommand);
+    string Input;
+    getline(cin, Input);
 
-    if(Commands.find(inputCommand) != Commands.end())
+    string Command;
+    vector<string> Args;
+
+    SplitInput(Input, Command, Args);
+
+    if(Commands.find(Command) != Commands.end())
     {
-      void (*p)() = Execute[inputCommand];
-      p();
+      void (*p)(vector<string>&) = Execute[Command];
+      p(Args);
     }
     else
-      cout << inputCommand << ": " << "command not found" << endl;
+      cout << Input << ": " << "command not found" << endl;
   }
 }
