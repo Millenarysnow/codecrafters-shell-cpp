12d28e22fcf6c2209891ae763372dfb98f356c76
millenarysnow <1371684195@qq.com>
2026-01-14 15:58:29 +0800

    codecrafters submit [skip ci]

--- src/cd.cpp
+++ src/cd.cpp
@@ -16,6 +16,14 @@ void MyShell::Cd::Execute(std::vector<std::string> &Args)
     int ret = 0;
 
 #ifdef _WIN32
+    if(!Args[0].empty())
+    {
+        ret = _chdir(Args[0].c_str());
+    }
+    else
+    {
+        ret = -1;
+    }
 #else
     if(!Args[0].empty())
     {
